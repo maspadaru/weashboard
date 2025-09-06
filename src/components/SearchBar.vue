650b4adf0ed2089f1e7a236b118d598c4f642a29
Mike Spadaru <mspadaru@gmail.com>
2025-09-06 18:07:57 +0200

    added some components

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import {ref} from 'vue';
+  const cityInputText = ref('');
+  const emit = defineEmits(['cityName'])
+
+  function onInput(): void {
+    emit('cityName', cityInputText.value)
+  }
+
+</script>
+
+<template>
+  <input v-model="cityInputText" @input="onInput()" placeholder="City"/>
+</template>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  defineProps({
+    cityName:String,
+  })
+</script>
+
+<template>
+    <p>Hello Dashboard {{ cityName }}</p>
+</template>
